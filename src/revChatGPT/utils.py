531bc0a1a466cf208cb680f820edabc74308cbaa
JeanCarlos Roman <digitalpixelgroup@gmail.com>
2024-05-14 23:22:19 -0400

    Update utils.py
    found a few adjustments that needed to be made on my end.

--- src/revChatGPT/utils.py
+++ src/revChatGPT/utils.py
@@ -1,8 +1,6 @@
 import re
-from typing import Set
-
-from prompt_toolkit import prompt
-from prompt_toolkit import PromptSession
+from typing import Set, List
+from prompt_toolkit import prompt, PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.history import InMemoryHistory
@@ -10,27 +8,28 @@ from prompt_toolkit.key_binding import KeyBindings
 
 bindings = KeyBindings()
 
-
 def create_keybindings(key: str = "c-@") -> KeyBindings:
     """
     Create keybindings for prompt_toolkit. Default key is ctrl+space.
-    For possible keybindings, see: https://python-prompt-toolkit.readthedocs.io/en/stable/pages/advanced_topics/key_bindings.html#list-of-special-keys
+    For possible keybindings, see:
+    https://python-prompt-toolkit.readthedocs.io/en/stable/pages/advanced_topics/key_bindings.html#list-of-special-keys
     """
-
     @bindings.add(key)
-    def _(event: dict) -> None:
+    def _(event) -> None:
         event.app.exit(result=event.app.current_buffer.text)
-
     return bindings
 
-
 def create_session() -> PromptSession:
+    """
+    Create a PromptSession with in-memory history.
+    """
     return PromptSession(history=InMemoryHistory())
 
-
-def create_completer(commands: list, pattern_str: str = "$") -> WordCompleter:
+def create_completer(commands: List[str], pattern_str: str = "$") -> WordCompleter:
+    """
+    Create a WordCompleter with a custom pattern.
+    """
     return WordCompleter(words=commands, pattern=re.compile(pattern_str))
-
 
 def get_input(
     session: PromptSession = None,
@@ -40,35 +39,31 @@ def get_input(
     """
     Multiline input function.
     """
-    return (
-        session.prompt(
+    if session:
+        return session.prompt(
             completer=completer,
             multiline=True,
             auto_suggest=AutoSuggestFromHistory(),
             key_bindings=key_bindings,
         )
-        if session
-        else prompt(multiline=True)
-    )
-
+    else:
+        return prompt(multiline=True)
 
 async def get_input_async(
     session: PromptSession = None,
     completer: WordCompleter = None,
 ) -> str:
     """
-    Multiline input function.
+    Asynchronous multiline input function.
     """
-    return (
-        await session.prompt_async(
+    if session:
+        return await session.prompt_async(
             completer=completer,
             multiline=True,
             auto_suggest=AutoSuggestFromHistory(),
         )
-        if session
-        else prompt(multiline=True)
-    )
-
+    else:
+        return prompt(multiline=True)
 
 def get_filtered_keys_from_object(obj: object, *keys: str) -> Set[str]:
     """
@@ -76,17 +71,15 @@ def get_filtered_keys_from_object(obj: object, *keys: str) -> Set[str]:
     :param keys: List of keys to include. If the first key is "not", the remaining keys will be removed from the class keys.
     :return: List of class keys.
     """
-    class_keys = obj.__dict__.keys()
+    class_keys = set(obj.__dict__.keys())
     if not keys:
-        return set(class_keys)
+        return class_keys
 
-    # Remove the passed keys from the class keys.
     if keys[0] == "not":
         return {key for key in class_keys if key not in keys[1:]}
-    # Check if all passed keys are valid
-    if invalid_keys := set(keys) - class_keys:
-        raise ValueError(
-            f"Invalid keys: {invalid_keys}",
-        )
-    # Only return specified keys that are in class_keys
+    
+    invalid_keys = set(keys) - class_keys
+    if invalid_keys:
+        raise ValueError(f"Invalid keys: {invalid_keys}")
+    
     return {key for key in keys if key in class_keys}
